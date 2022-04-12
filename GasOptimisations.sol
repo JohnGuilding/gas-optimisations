c1c46e8fb7ad8e01fbe5bf507b2b688da0b2cc75
JohnGuilding <jwguilding@gmail.com>
2022-04-12 21:09:50 +0100

    Add comments outlining optimisation techniques

--- GasOptimisations.sol
+++ GasOptimisations.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.13;
 
 contract GasOptimisations {
+    // This contract is a demo of gas optimisations on a solidity function 
+    // Tested in Remix
+
+    // start -  gas
+    // use calldata -  gas
+    // load state variables to memory () -  gas
+    // short circuit () -  gas
+    // loop increments () -  gas
+    // cache array length () -  gas
+    // load array elements to memory () -  gas
+
 
 }

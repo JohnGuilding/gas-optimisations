63b89438ded1bacd2e52d6f63ff59a3baf037192
JohnGuilding <jwguilding@gmail.com>
2022-04-12 21:11:54 +0100

    Add original function and getTotal function

--- GasOptimisations.sol
+++ GasOptimisations.sol
@@ -13,5 +13,21 @@ contract GasOptimisations {
     // cache array length () -  gas
     // load array elements to memory () -  gas
 
+    uint public total;
 
+    // [1, 3, 4, 7, 8, 13, 46, 77, 80, 99]
+    function multiplyIfOddAndTheNumber7Original(uint[] memory array) external {
+        for (uint i = 0; i < array.length; i += 1) {
+            bool isOdd = array[i] % 2 == 1;
+            bool isNumber7 = array[i] == 7;
+
+            if (isOdd && isNumber7) {
+                total += array[i];
+            }
+        }
+    }
+
+    function getTotal() external view returns (uint) {
+        return total;
+    }
 }
